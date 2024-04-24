b89b90a74ca42bd91c32de8f5246f074e37e1f39
Michaela M <masarova.michaela@gmail.com>
2024-04-24 13:37:11 +0200

    Chode: added libft functions

---
+++ libft/ft_strcpy.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+char *ft_strcpy(char *dest, const char *src)
+{
+    char *saved;
+
+	saved = dest;
+    while (*src)
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = 0;
+    return saved;
+}
