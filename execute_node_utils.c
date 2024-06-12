92926b981ac10d1be707196e5fcdd9467ecba605
Michaela M <masarova.michaela@gmail.com>
2024-06-13 01:07:18 +0300

    Fix: echo -nn + spliting variables

--- execute_node_utils.c
+++ execute_node_utils.c
@@ -329,7 +329,7 @@ void	handle_node_data(t_parse_tree **node, t_exec_vars *vars, t_env **env)
 		return(printf_global_error(1, 2, "my(s)hell: ft_strdup error\n"));
 	}
     index = vars->i;
-    if ((*node)->data->lexeme[0] == '$' && ft_strchr(vars->args[vars->i], ' '))
+    if ((*node)->data->lexeme[0] == '$' && ft_strchr(vars->args[vars->i], ' ') && !ft_strchr(vars->args[vars->i], '\''))
         vars->i = split_variable(vars->args[vars->i], vars->i, vars);//ADD ERROR HANDLING
     while (index <= vars->i)
     {
