dc8ab952c6d3da1bd3bb8e6da13c177de4606f6d
daria <d.padenkova@gmail.com>
2024-06-14 00:17:07 +0900

    fix: in vars creation control is modified

--- execute_node_utils.c
+++ execute_node_utils.c
@@ -329,8 +329,8 @@ void	handle_node_data(t_parse_tree **node, t_exec_vars *vars, t_env **env)
 		return(printf_global_error(1, 2, "my(s)hell: ft_strdup error\n"));
 	}
     index = vars->i;
-    if ((*node)->data->lexeme[0] == '$' && ft_strchr(vars->args[vars->i], ' ') && !ft_strchr(vars->args[vars->i], '\''))
-        vars->i = split_variable(vars->args[vars->i], vars->i, vars);//ADD ERROR HANDLING
+    //if ((*node)->data->lexeme[0] == '$' && ft_strchr(vars->args[vars->i], ' ') && !ft_strchr(vars->args[vars->i], '\''))
+        //vars->i = split_variable(vars->args[vars->i], vars->i, vars);//ADD ERROR HANDLING
     while (index <= vars->i)
     {
         handle_quotes_glob(&vars->args[index], env, &vars->error);
