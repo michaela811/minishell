d0553766cefb61ce8c1787b9b3c0ab7cab655b7c
daria <d.padenkova@gmail.com>
2024-06-13 22:57:08 +0900

    refactor: line argument dissmissed

--- exec_builtins_utils.c
+++ exec_builtins_utils.c
@@ -1,10 +1,10 @@
 #include "minishell.h"
 
-int	change_directory_and_update(char *path, t_env **env, char *cwd, int line)
+int	change_directory_and_update(char *path, t_env **env, char *cwd)//, int line)
 {
 	if (chdir(path) != 0)
 	{
-		printf_global_error(1, 2, "my(s)hell: line %d: cd: %s: No such file or directory\n", line, path);
+		printf_global_error(1, 2, "my(s)hell: line 2: cd: %s: No such file or directory\n", path);
 		/*g_last_exit_status = 1;
 		perror("chdir");*/
 		free(cwd);
