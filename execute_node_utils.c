95740ed844a326ae189300ec4ff11340f583b140
Michaela M <masarova.michaela@gmail.com>
2024-06-13 10:32:56 +0300

    Fix: echo when splitting with '"' or \'

--- execute_node_utils.c
+++ execute_node_utils.c
@@ -329,8 +329,12 @@ void	handle_node_data(t_parse_tree **node, t_exec_vars *vars, t_env **env)
 		return(printf_global_error(1, 2, "my(s)hell: ft_strdup error\n"));
 	}
     index = vars->i;
-    if ((*node)->data->lexeme[0] == '$' && ft_strchr(vars->args[vars->i], ' ') && !ft_strchr(vars->args[vars->i], '\''))
+    if ((*node)->data->lexeme[0] == '$' && ft_strchr(vars->args[vars->i], ' '))
+    {
+        if (ft_strchr(vars->args[vars->i], '\''))
+            handle_quotes_glob(&vars->args[vars->i], env, &vars->error);
         vars->i = split_variable(vars->args[vars->i], vars->i, vars);//ADD ERROR HANDLING
+    }
     while (index <= vars->i)
     {
         handle_quotes_glob(&vars->args[index], env, &vars->error);
