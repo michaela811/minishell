d9c0bb1a2866be85a46dc3e300b2b0721434082c
Michaela M <masarova.michaela@gmail.com>
2024-07-17 16:10:58 +0200

    Chore: norm and shortening of the functions

--- pipeline_here_doc.c
+++ pipeline_here_doc.c
@@ -6,36 +6,11 @@
 /*   By: mmasarov <mmasarov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 10:38:21 by mmasarov          #+#    #+#             */
-/*   Updated: 2024/07/16 16:32:27 by mmasarov         ###   ########.fr       */
+/*   Updated: 2024/07/17 16:10:12 by mmasarov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/*int	pipe_heredoc(t_p_tree **node, t_exec_vars *vars, t_hd_data *here_docs)
-{
-	char	filename[] = "/tmp/heredocXXXXXX";
-	int		fd;
-
-	fd = mkstemp(filename);
-	if (fd == -1)
-	{
-		vars->error = 1;
-		return (print_err(1, 2, "my(s)hell: mkstemp failure\n"), 1);
-	}
-	pipe_get_heredoc(node, vars, fd);
-	if (vars->error)
-	{
-		close(fd);
-		unlink(filename);
-		return (1);
-	}
-	lseek(fd, 0, SEEK_SET);
-	here_docs->fd = fd;
-	vars->i++;
-	unlink(filename);
-	return (0);
-}*/
 
 int	pipe_heredoc(t_p_tree **node, t_exec_vars *vars, t_hd_data *here_docs)
 {
@@ -51,7 +26,6 @@ int	pipe_heredoc(t_p_tree **node, t_exec_vars *vars, t_hd_data *here_docs)
 		unlink(filename);
 		return (1);
 	}
-	//lseek(fd, 0, SEEK_SET);
 	close(fd);
 	fd = open(filename, O_RDONLY);
 	here_docs->fd = fd;
@@ -60,70 +34,85 @@ int	pipe_heredoc(t_p_tree **node, t_exec_vars *vars, t_hd_data *here_docs)
 	return (0);
 }
 
-int	pipe_get_heredoc(t_p_tree **node, t_exec_vars *vars, int fd)
+static int	get_stdin(void)
 {
-	char			*buffer;
-	//int			file;
-	char			*line;
-	char			*contents = NULL;
+	int				original_stdin;
+	int				tty_fd;
 	struct termios	term;
 
-	(*node)->data->lexeme = handle_quotes_echo
-		((*node)->data->lexeme, &vars->error);
-	int original_stdin = dup(STDIN_FILENO);
+	original_stdin = dup(STDIN_FILENO);
 	if (original_stdin == -1)
 	{
-	   	perror("my(s)hell: dup");
-	  	return 1;
+		perror("my(s)hell: dup");
+		return (1);
 	}
 	if (tcgetattr(STDIN_FILENO, &term) == -1)
 	{
-		int tty_fd = open("/dev/tty", O_RDONLY);
+		tty_fd = open("/dev/tty", O_RDONLY);
 		if (tty_fd == -1)
-		{
-			perror("my(s)hell: open /dev/tty");
-			close(original_stdin);
-			return 1;
-		}
+			return (perror("my(s)hell: open /dev/tty"), close(original_stdin),
+				1);
 		if (dup2(tty_fd, STDIN_FILENO) == -1)
-		{
-			perror("my(s)hell: dup2 tty_fd to STDIN_FILENO");
-			close(tty_fd);
-			close(original_stdin);
-			return 1;
-		}
+			return (perror("my(s)hell: dup2 tty_fd to STDIN_FILENO"),
+				close(tty_fd), close(original_stdin), 1);
 		close(tty_fd);
 	}
+	return (0);
+}
+
+char	*get_heredoc_content(char *contents, char *buffer)
+{
+	char	*temp;
+
+	temp = NULL;
+	if (contents == NULL)
+	{
+		contents = malloc(ft_strlen(buffer) + 2);
+		if (contents == NULL)
+			return (free(buffer), NULL);
+		ft_strcpy(contents, buffer);
+	}
+	else
+	{
+		temp = realloc(contents, ft_strlen(contents) + ft_strlen(buffer) + 2);
+		if (temp == NULL)
+			return (free(contents), free(buffer), NULL);
+		contents = temp;
+		ft_strcat(contents, buffer);
+	}
+	return (contents);
+}
+
+static int	is_it_delimiter(t_p_tree **node, char *buffer)
+{
+	if (ft_exact_strcmp(buffer, (*node)->data->lexeme) == 0)
+	{
+		free(buffer);
+		return (1);
+	}
+	return (0);
+}
+int	pipe_get_heredoc(t_p_tree **node, t_exec_vars *vars, int fd)
+{
+	char			*buffer;
+	char			*line;
+	char			*contents;
+
+	contents = NULL;
+	(*node)->data->lexeme = handle_quotes_echo
+		((*node)->data->lexeme, &vars->error);
+	if (get_stdin())
+		return (1);
 	while (1)
 	{
-		//buffer = readline("heredoc> ");
-		//if (buffer == NULL)
-		//	break ;
-		line = get_next_line(fileno(stdin)); // for tester from this line
+		line = get_next_line(fileno(stdin));
 		if (line == NULL)
 			break ;
 		buffer = ft_strtrim(line, "\n");
-		free(line); // to this line
-		if (ft_exact_strcmp(buffer, (*node)->data->lexeme) == 0)
-		{
-			free(buffer);
+		free(line);
+		if (is_it_delimiter(node, buffer))
 			break ;
-		}
-		if (contents == NULL)
-		{
-			contents = malloc(ft_strlen(buffer) + 2);
-			if (contents == NULL)
-				return (free(buffer), 1);
-			ft_strcpy(contents, buffer);
-		}
-		else
-		{
-			char *temp = realloc(contents, ft_strlen(contents) + ft_strlen(buffer) + 2);
-			if (temp == NULL)
-				return (free(contents), free(buffer), 1);
-			contents = temp;
-			ft_strcat(contents, buffer);
-		}
+		contents = get_heredoc_content(contents, buffer);
 		ft_strcat(contents, "\n");
 		free(buffer);
 	}
@@ -133,9 +122,39 @@ int	pipe_get_heredoc(t_p_tree **node, t_exec_vars *vars, int fd)
 		free(contents);
 	}
 	return (0);
-}
+} //for tester
 
-void init_heredocs(t_hd_data **here_docs)
+/* FOR EVALUATION
+int	pipe_get_heredoc(t_p_tree **node, t_exec_vars *vars, int fd)
+{
+	char			*buffer;
+	char			*contents;
+
+	contents = NULL;
+	(*node)->data->lexeme = handle_quotes_echo
+		((*node)->data->lexeme, &vars->error);
+	if (get_stdin())
+		return (1);
+	while (1)
+	{
+		buffer = readline("heredoc> ");
+		if (buffer == NULL)
+			break ;
+		if (is_it_delimiter(node, buffer))
+			break ;
+		contents = get_heredoc_content(contents, buffer);
+		ft_strcat(contents, "\n");
+		free(buffer);
+	}
+	if (contents != NULL)
+	{
+		write(fd, contents, strlen(contents));
+		free(contents);
+	}
+	return (0);
+}*/
+
+void	init_heredocs(t_hd_data **here_docs)
 {
 	if (*here_docs == NULL)
 	{
@@ -153,9 +172,10 @@ void init_heredocs(t_hd_data **here_docs)
 
 int	is_there_here_doc(t_p_tree **tree, t_hd_data **here_docs)
 {
-	t_p_tree    *current = *tree;
-	t_exec_vars *vars;
+	t_p_tree	*current;
+	t_exec_vars	*vars;
 
+	current = *tree;
 	vars = malloc(sizeof(t_exec_vars));
 	if (!vars)
 		return (print_err(1, 2, "my(s)hell: execute_node malloc error\n"), 1);
