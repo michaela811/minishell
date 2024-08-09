46f9ddc0c56039fd2e0adf92278b8e5fffa2a3c5
Michaela M <masarova.michaela@gmail.com>
2024-08-09 12:28:25 +0200

    Fix: added underscore

--- handle_dollar_sign_utils.c
+++ handle_dollar_sign_utils.c
@@ -6,7 +6,7 @@
 /*   By: mmasarov <mmasarov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 11:40:06 by mmasarov          #+#    #+#             */
-/*   Updated: 2024/08/09 11:40:18 by mmasarov         ###   ########.fr       */
+/*   Updated: 2024/08/09 12:22:53 by mmasarov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*find_first_non_alnum(char *str)
 {
 	while (*str)
 	{
-		if (!isalnum((unsigned char)*str))
+		if (!isalnum((unsigned char)*str) && *str != '_')
 			return (str);
 		str++;
 	}
